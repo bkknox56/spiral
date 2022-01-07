a573d39d3fb71cff490601da6dfc2c964ca932e8
bkknox46 <bkknox56@gmail.com>
2022-01-07 18:48:25 +0530

    code is ready

---
+++ src/og/git/SampleCode.java
@@ -0,0 +1,13 @@
+package og.git;
+
+public class SampleCode {
+
+		private void empId() {
+			
+			System.out.println("empId");
+		}
+		private void empName() {
+			
+			System.out.println("empName");
+		}
+}
